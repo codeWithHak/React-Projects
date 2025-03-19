65d650675312075b2e086582fd04284daa926a28
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2025-03-19 17:47:48 +0500

    fecth second weather api

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,48 +1,109 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
-const key = "aecf17d11b9a1bf1cfb719e72f11b477"
-const [data,setData] = useState()
-const [error,setError] = useState()
-const [isLoading, setIsLoading] = useState(true)
+  const key = "78c1eee980b22b39fde9fe5d79766b76";
+  const inputRef = useRef(null)
+  
+  const [data, setData] = useState();
+  const [error, setError] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  // const [longitude, setLongitude] = useState();
+  // const [latitude, setLatitude] = useState();
+  const [city, setCity] = useState();
 
-  const fetchLatitude = async ()=>{
+  useEffect(()=>{
+      inputRef.current.focus()
+  },[])
 
-try{
-  const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid=${key}`)
-  if (!response.ok){
-    throw new Error("Failed to fetch API response.")
+  const handleEvent = (e)=>{
+    if (e.key === "Enter"){
+      fetchLatitude()
+      fetchWeather()
+    }
   }
-  const json = await response.json()
-  console.log(json);
-  setData(json)
-  setIsLoading(false)
-}
-catch(error){
-  setError(error.message)
-  console.error(error)
-}
+
+  // This will fetch the latitude which we later convert to city
+  const fetchLatitude = async () => {
+    try {
+      setIsLoading(true)
+      const response = await fetch(
+        `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${key}`
+      );
+      if (!response.ok) {
+        throw new Error("Error occured while fetching API");
+      }
+      const json = await response.json();
+      if (json.length < 1) {
+        setIsLoading(false)
+        throw new Error("City not found")
+      }
+        console.log(json);
+        
+        setIsLoading(false)
+      
+      
+      
+    } catch (error) {
+      setError(error.message);
+      console.error(error);
+    }
+  
+  }
+  
+const fetchWeather = async ()=>{
+
+  const lat = data.map((weather)=>weather.lat)
+  const lon = data.map((weather)=>weather.lon)
+
+  try{
+    setIsLoading(true)
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`)
+    if(!res.ok){
+      throw new Error("Failed to fetch weather")
+    }
+    const json = res.json()
+    setIsLoading(false)
+    setData(json)
+    console.log(json);
+    
+  }
+  catch(error){
+    setError(error.message)
+  }
+  
 }
 
-useEffect(()=>{
+const handleFecth = ()=>{
   fetchLatitude()
-},[])
+  fetchWeather()
+}
 
-
-if(error) return <p>Error...{error}</p>
-if (isLoading) return <p>Loading...</p>
+  // if (error) return <p>Error...{error}</p>;
+ 
   return (
     <>
-     <h1>Weather app</h1>
-     {data.map((weather)=>(
-      <>
-      <p key={weather.name}>{weather.name}</p>
-      <p key={weather.lat}>{weather.lat}</p>
-      <p key={weather.lon}>{weather.lon}</p>
-    </>))}
+      <h1>Weather app</h1>
+      <input ref={inputRef} type="text" value={city} onChange={(e)=>setCity(e.target.value)} onKeyDown={handleEvent}/>
+      <button onClick={handleFecth}>Search</button>
+      {
+        isLoading && <p>Loading...</p>
+      }
+
+      {
+        error && <p>{error}</p>
+      }
+
+      {data && !isLoading && (
+        
+        <div key={data.name}>
+          <p >Temp:{data.main.temp}</p>
+          </div>
+        
+      )}
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
