95d63d07b117fb83d5081d66b6d804e56d79b801
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2025-04-08 00:10:03 +0500

    fix copy button

--- 03_password_generator/src/App.jsx
+++ 03_password_generator/src/App.jsx
@@ -1,33 +1,59 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState, useRef } from "react";
 
 function App() {
+
+  console.log("Component mounted");
+  
   const [length, setLength] = useState(8);
-  const [isCharAllowed, setIsCharAllowed] = useState(false);
-  const [isNumAllowed, setIsNumAllowed] = useState(false);
+  const [isCharAllowed, setIsCharAllowed] = useState(true);
+  const [isNumAllowed, setIsNumAllowed] = useState(true);
   const [password, setPassword] = useState("");
+  const [copyButton, setCopyButton] = useState(true)
   console.log("Before password generator");
   
+
+
+
+  let inputRef = useRef(null)
+
+  const copyToClipboard = useCallback(()=>{
+    inputRef?.current.select()
+    window.navigator.clipboard.writeText(password)
+    setCopyButton((prev)=>!prev)
+  },[password])
+
   const passwordGenerator = useCallback(() => {
+    setCopyButton((prev)=>!prev)
     let pass = "";
     let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     if (isNumAllowed) str += "0123456789";
     if (isCharAllowed) str += "!@#$%^&*-_";
     console.log("Under pass gen outside loop");
     for (let i = 0; i < length; i++) {
-      let randomNumber = Math.floor(Math.random() * 1 + 1);
+
+      let randomNumber = Math.floor(Math.random() * str.length + 1);
+      console.log("randomNumber: ",randomNumber);
+      
       pass += str[randomNumber];
-      console.log("Random number: ", str[randomNumber]);
+      console.log("st[randomNumber]: ", str[randomNumber]);
       
     }
     setPassword(pass);
-    console.log(pass);
+    console.log('pass: ',pass);
 
   }, [length, isCharAllowed, isNumAllowed]);
 
   useEffect(() => {
     passwordGenerator();
-  }, {length, isCharAllowed, isNumAllowed});
+  }, [length, isCharAllowed, isNumAllowed]);
 
+  const handleLength = (e)=>{
+    setLength(e.target.value)
+    console.log("e.target.value: ",e.target.value)
+    
+    // console.log(e.target.value);
+    
+  }
   return (
     <div className="bg-gray-950 w-full h-screen py-10">
       <div className="bg-blue-900 px-4 py-4 max-w-lg mx-auto rounded-md">
@@ -37,19 +63,28 @@ function App() {
             value={password}
             placeholder="Passowrd"
             readOnly
+            ref={inputRef}
             className="outline-none bg-white py-2 px-4 rounded text-gray-950 w-full"
           />
-          <button className="bg-blue-800 px-2 py-2 rounded">copy</button>
+          {copyButton?
+             <button className="bg-blue-800 px-2 py-2 rounded" onClick={copyToClipboard}>Copy</button>
+             
+             :
+             <button className="bg-blue-800 px-2 py-2 rounded" onClick={copyToClipboard}>Copied</button>
+             
+             }
+          
         </div>
         <div className="flex justify-between px-3 mt-4 text-white">
           <div>
             <input
               type="range"
-              id="length"
-              min={8}
-              max={50}
+              // id="length"
+              min='8'
+              max='50'
               value={length}
-              onChange={(e) => setLength(e.target.value)}
+              log
+              onChange={handleLength}
             />
             <label htmlFor="length">Length {length}</label>
           </div>
@@ -58,7 +93,6 @@ function App() {
               type="checkbox"
               id="numbers"
               defaultChecked={isNumAllowed}
-              value={isNumAllowed}
               onChange={() => setIsNumAllowed(prev => !prev)}
             />
             <label htmlFor="numbers">Numbers</label>
@@ -67,8 +101,7 @@ function App() {
             <input
               type="checkbox"
               id="characters"
-              defaultChecked={isNumAllowed}
-              value={isCharAllowed}
+              defaultChecked={isCharAllowed}
               onChange={() => setIsCharAllowed(prev => !prev)}
             />
             <label htmlFor="characters">Characters</label>
